cc518690eeec2c516d5e5a90344659efd08dd4dc
Casper Dik <casperdik3@gmail.com>
2020-05-12 08:59:17 +0200

    quantecon

--- QuantEcon/exercises_chapter3/ex2.py
+++ QuantEcon/exercises_chapter3/ex2.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 T = 200
 alphas = [0, 0.8, 0.98]
 x = np.empty(T+1)
+x[0] = 0
 
 for alpha in alphas:
-    x[0] = 0
     for t in range(T):
         error_term = np.random.randn()
         x[t+1] = alpha * x[t] + error_term
-    plt.plot(x)
+    plt.plot(x, label= "alpha = " + str(alpha))
 
+plt.legend()
 plt.show()
