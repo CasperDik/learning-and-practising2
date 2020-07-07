b034aaf5b16ae1e63357dec00dc37975a8d093e5
Casper Dik <casperdik3@gmail.com>
2020-07-07 15:44:06 +0200

    pandas

---
+++ learning-and-practising/pandas/pd-colums.py
@@ -0,0 +1,22 @@
+#https://www.learndatasci.com/tutorials/python-pandas-tutorial-complete-introduction-for-beginners/
+import pandas as pd
+
+movie_df = pd.read_csv("IMDB-Movie-Data.csv", index_col="Title")
+
+print(movie_df.columns)
+
+#rename column names
+movie_df.rename(columns={
+        'Runtime (Minutes)': 'Runtime',
+        'Revenue (Millions)': 'Revenue_millions'
+    }, inplace=True)
+
+print(movie_df.columns)
+
+#rename all columns with list
+movie_df.columns = ['rank', 'genre', 'description', 'director', 'actors', 'year', 'runtime', 'rating', 'votes', 'revenue_millions', 'metascore']
+print(movie_df.columns)
+
+#rename all column name to lowercase
+movie_df.columns = [col.lower() for col in movie_df]
+print(movie_df.columns)
