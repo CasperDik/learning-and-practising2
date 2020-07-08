2aa06fd5e8c42b8fa0a4df6e6c10869659573163
Casper Dik <casperdik3@gmail.com>
2020-07-08 12:01:52 +0200

    pandas

--- learning-and-practising/pandas/pd-plotting.py
+++ learning-and-practising/pandas/pd-plotting.py
@@ -1,12 +1,9 @@
 import pandas as pd
-import matplotlib as plt
+from matplotlib import pyplot as plt
 
 movies_df = pd.read_csv("IMDB-Movie-Data.csv", index_col="Title")
 movies_df.columns = ['rank', 'genre', 'description', 'director', 'actors', 'year', 'runtime', 'rating', 'votes', 'revenue_millions', 'metascore']
 
-plt.rcParams.update({'font.size': 20, 'figure.figsize': (10, 8)})       #set fond and plot size
-
 # plot
 movies_df.plot(kind="scatter", x="rating", y="revenue_millions", title="Revenue vs rating")
-
 plt.show()
