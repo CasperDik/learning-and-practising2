3b50fe8020f4cd25c77accbc2bd378810f2b9f34
Casper Dik <casperdik3@gmail.com>
2020-07-28 21:15:14 +0200

    edX

---
+++ learning-and-practising/HavardX_Using-python-for-reserch/HW1/ex2.py
@@ -0,0 +1,14 @@
+# a
+import math
+print(math.pi/4)
+
+# b
+import random
+
+random.seed(1) # Fixes the see of the random number generator.
+
+def rand():
+    rand = random.uniform(-1,1)
+    return rand
+
+rand()
