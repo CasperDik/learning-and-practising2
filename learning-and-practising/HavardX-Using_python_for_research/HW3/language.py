6b45a70728bc6478f44616f963b9174f5c4dbc94
Casper Dik <casperdik3@gmail.com>
2020-08-04 12:14:59 +0200

    edX

--- learning-and-practising/HavardX-Using_python_for_research/HW3/language.py
+++ learning-and-practising/HavardX-Using_python_for_research/HW3/language.py
@@ -1,6 +1,7 @@
 from collections import Counter
-
-text1 = "This is my test text. We're keeping this text short to keep things manageable"
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def count_words(text):
     text = text.lower()
@@ -34,6 +35,42 @@ def read_book(title_path):
         text = text.replace("\n", "").replace("\r", "")
     return text
 
-text = read_book("./English/shakespeare/Romeo and Juliet.txt")
+def word_stats(word_counts):
+    num_unique = len(word_counts)
+    counts = word_counts.values()
+    return (num_unique, counts)
 
 
+book_dir = "./Books"
+stats = pd.DataFrame(columns = ("language", "author", "title", "length", "unique"))
+title_num = 1
+
+for language in os.listdir(book_dir):
+    for author in os.listdir(book_dir + "/" + language):
+        for title in os.listdir(book_dir + "/" + language + "/" + author):
+            inputfile = book_dir + "/" + language + "/" + author + "/" + title
+            print(inputfile)
+            text = read_book(inputfile)
+            (num_unique, counts) = word_stats(count_words(text))
+            stats.loc[title_num] = language, author.capitalize(), title.replace(".txt", ""), sum(counts), num_unique
+            title_num += 1
+
+
+plt.figure(figsize = (10, 10))
+
+subset = stats[stats.language == "English"]
+plt.loglog(subset.length, subset.unique, "o", label = "English", color = "crimson")
+
+subset = stats[stats.language == "French"]
+plt.loglog(subset.length, subset.unique, "o", label = "French", color = "forestgreen")
+
+subset = stats[stats.language == "German"]
+plt.loglog(subset.length, subset.unique, "o", label = "German", color = "orange")
+
+subset = stats[stats.language == "Portuguese"]
+plt.loglog(subset.length, subset.unique, "o", label = "Portuguese", color = "blueviolet")
+
+plt.legend()
+plt.xlabel("Book length")
+plt.ylabel("Number of unique words")
+plt.show()
