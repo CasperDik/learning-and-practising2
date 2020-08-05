3ed7e539b5d4e049acba9e928605178c0e1cd68a
Casper Dik <casperdik3@gmail.com>
2020-08-05 14:15:34 +0200

    edX

--- learning-and-practising/HavardX-Using_python_for_research/HW3/CaseStudy_Hamlet.py
+++ learning-and-practising/HavardX-Using_python_for_research/HW3/CaseStudy_Hamlet.py
@@ -21,12 +21,60 @@ hamlets = pd.read_csv("hamlet.csv", index_col=0)
 language, text = hamlets.iloc[0]
 counted_text = count_words_fast(text)
 
-data = pd.DataFrame(columns=("word", "count"))
+data = pd.DataFrame(columns=("word", "count", "length", "frequency"))
 keys = list(counted_text.keys())
 values = list(counted_text.values())
 
 for i in range(len(keys)):
-    data.loc[i] = keys[i], values[i]
+    if values[i] > 10:
+        frequency = "frequent"
+    elif values[i] <= 10 and values[i] > 1:
+        frequency = "infrequent"
+    else:
+        frequency = "unique"
+
+    data.loc[i] = keys[i], values[i], len(keys[i]), frequency
 
 data = data.sort_values(by=["count"], ascending=False)
-# check if keys and values belong together with counted_text
+print(data.loc[data["word"]=="hamlet"])
+print(data[data["frequency"] == "unique"].count())
+
+
+subset = pd.DataFrame(columns=("language", "frequency", "mean_word_length", "num_words"))
+
+freq = ["frequent", "infrequent", "unique"]
+
+mean = [data[data["frequency"]==f].mean().get("length") for f in freq]
+num_word = [data[data["frequency"]==f].count().get("length") for f in freq]
+
+for j in range(len(freq)):
+    subset.loc[j] = language, freq[j], mean[j], num_word[j]
+
+
+def summarize_text(language, text):
+    counted_text = count_words_fast(text)
+
+    data = pd.DataFrame({
+        "word": list(counted_text.keys()),
+        "count": list(counted_text.values())
+    })
+
+    data.loc[data["count"] > 10, "frequency"] = "frequent"
+    data.loc[data["count"] <= 10, "frequency"] = "infrequent"
+    data.loc[data["count"] == 1, "frequency"] = "unique"
+
+    data["length"] = data["word"].apply(len)
+
+    sub_data = pd.DataFrame({
+        "language": language,
+        "frequency": ["frequent", "infrequent", "unique"],
+        "mean_word_length": data.groupby(by="frequency")["length"].mean(),
+        "num_words": data.groupby(by="frequency").size()
+    })
+
+    return (sub_data)
+
+
+for i in range(3):
+    language, text = hamlets.iloc[i]
+    summarize_text(language, text)
