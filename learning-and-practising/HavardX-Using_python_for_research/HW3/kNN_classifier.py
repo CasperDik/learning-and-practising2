fd20057806049bb07ad0b26001cae06c616d0406
Casper Dik <casperdik3@gmail.com>
2020-08-11 13:22:11 +0200

    edX

--- learning-and-practising/HavardX-Using_python_for_research/HW3/kNN_classifier.py
+++ learning-and-practising/HavardX-Using_python_for_research/HW3/kNN_classifier.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random
 import scipy.stats as ss
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
 
 def distance(p1, p2):
     return np.sqrt(np.sum(np.power(p2 - p1, 2)))
@@ -37,10 +39,48 @@ def knn_predict(p, points, outcomes,  k=5):
     ind = find_nearest_neighbors(p, points, k)
     return majority_vote(outcomes[ind])
 
-points = np.array([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]])
-p = np.array([1.0, 2.7])
-# outcomes is classes of the points in points
-outcomes = np.array([0,0,0,0,1,1,1,1,1])
+def generate_synth_data(n=50):
+    """create two sets of points from bivariate normal distribution"""
+    points = np.concatenate((ss.norm(0,1).rvs((n,2)), ss.norm(1,1).rvs((n,2))), axis=0)
+    outcomes = np.concatenate((np.repeat(0, n), np.repeat(1, n)))
+    return (points, outcomes)
 
-x = knn_predict(p, points, outcomes, k=2)
-print(x)
+def make_prediction_grid(predictors, outcomes, limits, h, k):
+    """Classify each point on the prediction grid."""
+    (x_min, x_max, y_min, y_max) = limits
+    xs = np.arange(x_min, x_max , h)
+    ys = np.arange(y_min, y_max, h)
+    xx, yy = np.meshgrid(xs, ys)
+
+    prediction_grid = np.zeros(xx.shape, dtype=int)
+    for i,x in enumerate(xs):
+        for j, y in enumerate(ys):
+            p = np.array([x,y])
+            prediction_grid[j,i] = knn_predict(p, predictors, outcomes, k)
+
+    return (xx, yy, prediction_grid)
+
+def plot_prediction_grid (xx, yy, prediction_grid, filename):
+    """ Plot KNN predictions for every point on the grid."""
+    background_colormap = ListedColormap (["hotpink","lightskyblue", "yellowgreen"])
+    observation_colormap = ListedColormap (["red","blue","green"])
+    plt.figure(figsize =(10,10))
+    plt.pcolormesh(xx, yy, prediction_grid, cmap = background_colormap, alpha = 0.5)
+    plt.scatter(predictors[:,0], predictors [:,1], c = outcomes, cmap = observation_colormap, s = 50)
+    plt.xlabel('Variable 1'); plt.ylabel('Variable 2')
+    plt.xticks(()); plt.yticks(())
+    plt.xlim (np.min(xx), np.max(xx))
+    plt.ylim (np.min(yy), np.max(yy))
+    plt.savefig(filename)
+    plt.show()
+
+
+
+(predictors, outcomes) = generate_synth_data()
+k = 5
+filename = "knn_synth_5.pdf"
+limits = (-3,4,-3,4)
+h = 0.1
+
+(xx, yy, prediction_grid) = make_prediction_grid(predictors, outcomes, limits , h, k)
+plot_prediction_grid(xx, yy, prediction_grid, filename)
