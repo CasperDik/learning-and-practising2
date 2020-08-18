c8b3a1eaf19fdb9b39f36e466912f3fa27d156e1
Casper Dik <casperdik3@gmail.com>
2020-08-18 15:04:47 +0200

    edX

---
+++ learning-and-practising/HavardX-Using_python_for_research/Week4/Birds_gps_tracking/bird_tracking.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+birddata = pd.read_csv("bird_tracking.csv")
+
+bird_names = pd.unique(birddata.bird_name)
+plt.figure(figsize=(7,7))
+for bird_name in bird_names:
+    ix = birddata.bird_name == bird_name
+    x, y = birddata.longitude[ix], birddata.latitude[ix]
+    plt.plot(x,y,".", label=bird_name)
+plt.xlabel("Longitude")
+plt.xlabel("Latitude")
+plt.legend(loc="lower right")
+plt.show()
