601b6cdfa9509578b103007bf30d042637303d38
Casper Dik <casperdik3@gmail.com>
2020-08-18 17:01:57 +0200

    edX

--- learning-and-practising/HavardX-Using_python_for_research/Week4/Birds_gps_tracking/bird_tracking.py
+++ learning-and-practising/HavardX-Using_python_for_research/Week4/Birds_gps_tracking/bird_tracking.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime
 
 birddata = pd.read_csv("bird_tracking.csv")
-
 bird_names = pd.unique(birddata.bird_name)
+
 plt.figure(figsize=(7,7))
 for bird_name in bird_names:
     ix = birddata.bird_name == bird_name
