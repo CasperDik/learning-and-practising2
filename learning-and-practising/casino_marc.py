25a4fc249ad230d0e32a040298e52376e41d137f
Casper Dik <casperdik3@gmail.com>
2021-01-25 17:24:04 +0100

    updated version

--- learning-and-practising/casino_marc.py
+++ learning-and-practising/casino_marc.py
@@ -1,14 +1,17 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-N = 10000
+t0 = time.time()
+
+N = 100000
 inleg = 1
 p_succes = 4/37
 multiplier_ifsucces = 9
 x1 = np.arange(0,N+1)
 
-for i in range(20):
+for j in range(2000):
     running_total = 0
     draw_running_total = []
     for i in range(1,N+2):
@@ -19,9 +22,10 @@ for i in range(20):
         else:
             running_total += -inleg
         draw_running_total.append(running_total/i)
-    print(draw_running_total[N])
+    print(draw_running_total[N], j)
     plt.plot(x1, draw_running_total, alpha=0.3)
 
 plt.hlines(0, 0, N, alpha=1)
 plt.show()
 
+print("time:", (time.time()-t0)/60)
