ecbf4cdfacec618462c31d9c1b3a82ce79570e75
Casper Dik <casperdik3@gmail.com>
2021-11-23 14:51:41 +0100

    cmc

---
+++ scrapy_cmc/cmc/cmc/spiders/cmc_scraper.py
@@ -0,0 +1,28 @@
+import scrapy
+from ..items import CmcItem
+
+class QuoteSpider(scrapy.Spider):
+    name = "crypto"
+    start_urls = [
+        "https://coinmarketcap.com/"
+    ]
+
+    def parse(self, response, **kwargs):
+        items = CmcItem()
+
+        name = response.css(".cmc-link .iworPT::text").extract()
+        price = response.css(".cLgOOr span::text").extract()
+        volume = response.css(".font_weight_500::text").extract()
+        marketcap = response.css(".ieFnWP::text").extract()
+        circulating_supply = response.css(".kZlTnE::text").extract()
+
+        count = 0
+        for _ in name:
+            items["name"] = name[count]
+            items["price"] = price[count]
+            items["volume"] = volume[count]
+            items["marketcap"] = marketcap[count]
+            items["circulating_supply"] = circulating_supply[count]
+            count += 1
+            yield items
+
