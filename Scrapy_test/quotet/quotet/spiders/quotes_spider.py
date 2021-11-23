8e06b5d16a9901cceb44c5700f13e9f104d54860
Casper Dik <casperdik3@gmail.com>
2021-11-23 13:47:20 +0100

    scrapy

--- Scrapy_test/quotet/quotet/spiders/quotes_spider.py
+++ Scrapy_test/quotet/quotet/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import QuotetItem
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
@@ -7,14 +8,19 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response, **kwargs):
+
+        items = QuotetItem()
+
         all_div_quotes = response.css("div.quote")
 
         for quotes in all_div_quotes:
             title = quotes.css("span.text::text").extract()
             author = quotes.css(".author::text").extract()
             tag = quotes.css(".tag::text").extract()
-            yield {
-                "title": title,
-                "author": author,
-                "tag": tag
-            }
+
+
+            items["title"] = title
+            items["author"] = author
+            items["tag"] = tag
+
+            yield items
